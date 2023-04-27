ec5377e204aac903cc507d8466cad44f2ebe0411
rasahil <sahilrangari31@gmail.com>
2023-04-27 11:54:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function red(){
+    document.getElementById("Stop").value;
+    document.getElementById("myimg").src="red.jpg";
+    document.getElementById("msg").innerHTML=Stop.value;
+}
+function orange(){
+    document.getElementById("Slow_down").value;
+    document.getElementById("myimg").src="orange.jpg";
+    document.getElementById("msg").innerHTML=Slow_down.value;
+}
+function green(){
+    document.getElementById("Go").value;
+    document.getElementById("myimg").src="green.jpg";
+    document.getElementById("msg").innerHTML=Go.value;
+}
